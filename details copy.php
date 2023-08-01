c2bc05799dba27042f5c9a6e8c9035d697eb2072
jiaenlow-rp <21024625@myrp.edu.sg>
2023-08-01 14:01:01 +0800

    updates to the code

--- details copy.php
+++ details copy.php
@@ -28,6 +28,7 @@ if (!empty($row)) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <script src="https://unpkg.com/vue@2.6.14/dist/vue.js"></script>
     <title>Hotel Details</title>
 </head>
 
@@ -48,55 +49,75 @@ if (!empty($row)) {
                     </ul>
                 </div>
         </nav>
+
         <?php if (!empty($id)) { ?>
-            <div style="width:350px;">
+            <div style="width: 350px;">
                 <?php if ($filetype == "LINK") { ?>
-                    <iframe src="<?php echo $content ?>">
-                    </iframe>
-                    <?php
-                } elseif ($filetype == "IMG") { ?>
-                    <iframe src="media/<?php echo $content ?>">
-                        </iframe>
-                    <?php ?>
+                    <!-- Vue component for LINK -->
+                    <div id="app">
+                        <iframe-component :src="<?php echo $content ?>"></iframe-component>
+                    </div>
+                <?php } elseif ($filetype == "IMG") { ?>
+                    <!-- Vue component for IMG -->
+                    <div id="app">
+                        <iframe-component :src="'media/<?php echo $content ?>'"></iframe-component>
+                    </div>
                 <?php } elseif ($filetype == 'MP3') { ?>
-                    <audio controls>
-                        <source src="media/<?php echo $content ?>" type='audio/mpeg'>
-                    </audio>
-                    <?php
-                } elseif ($filetype == "MP4") { ?>
-                    <video controls>
-                        <source src="media/<?php echo $content ?>" type='video/mp4'>
-                    </video>
-                    <?php
-                } elseif ($filetype == "pdf") { ?>
-                    <iframe src="media/<?php echo $content ?>">
-                    </iframe>
-                    <?php
-                } else { ?>
-                    <iframe src="<?php echo $content ?>">
-                    </iframe> 
-                    <?php
-                }
-                ?>
-                <br />
-                <b>File Type:</b>
-                <?php echo $filetype ?><br />
-                <b>Author:</b>
-                <?php echo $author ?><br />
-                <b>Publisher</b>
-                <?php echo $publisher ?><br />
-                <b>Date Published:</b>
-                <?php echo $date ?><br />
-                <b>Skill Tags:</b>
-                <?php echo $skilltags ?><br />
-                <b>Title:</b>
-                <?php echo $title ?><br />
-                <b>Duration:</b>
-                <?php echo $duration ?><br />
-                <b>Intent:</b>
-                <?php echo $intent ?><br />
+                    <!-- Vue component for MP3 -->
+                    <div id="app">
+                        <iframe-component :src="'media/<?php echo $content ?>'"></iframe-component>
+                    </div>
+                <?php } elseif ($filetype == "MP4") { ?>
+                    <!-- Vue component for MP4 -->
+                    <div id="app">
+                        <iframe-component :src="'media/<?php echo $content ?>'"></iframe-component>
+                    </div>
+                <?php } elseif ($filetype == "pdf") { ?>
+                    <!-- Vue component for PDF -->
+                    <div id="app">
+                        <iframe-component :src="'media/<?php echo $content ?>'"></iframe-component>
+                    </div>
+                <?php } else { ?>
+                    <!-- Vue component for default (fallback) -->
+                    <div id="app">
+                        <iframe-component :src="content"></iframe-component>
+                    </div>
+                <?php } ?>
             </div>
-        <?php } ?>
+
+            <script>
+                // Define the Vue iframe component
+                Vue.component('iframe-component', {
+                    props: ['src'],
+                    template: '<iframe :src="src"></iframe>'
+                });
+
+                // Initialize the Vue app
+                new Vue({
+                    el: '#app',
+                });
+            </script>
+
+
+            <br />
+            <b>File Type:</b>
+            <?php echo $filetype ?><br />
+            <b>Author:</b>
+            <?php echo $author ?><br />
+            <b>Publisher</b>
+            <?php echo $publisher ?><br />
+            <b>Date Published:</b>
+            <?php echo $date ?><br />
+            <b>Skill Tags:</b>
+            <?php echo $skilltags ?><br />
+            <b>Title:</b>
+            <?php echo $title ?><br />
+            <b>Duration:</b>
+            <?php echo $duration ?><br />
+            <b>Intent:</b>
+            <?php echo $intent ?><br />
+        </div>
+    <?php } ?>
 </body>
 
 </html>
