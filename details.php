f1703bf95bd40481e77f4e5301626979402bd39c
jiaenlow-rp <21024625@myrp.edu.sg>
2023-08-02 12:05:25 +0800

    cleaned up files

--- details.php
+++ details.php
@@ -28,6 +28,7 @@ if (!empty($row)) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <script src="https://unpkg.com/vue@2.6.14/dist/vue.js"></script>
     <title>Hotel Details</title>
 </head>
 
@@ -48,30 +49,75 @@ if (!empty($row)) {
                     </ul>
                 </div>
         </nav>
+
         <?php if (!empty($id)) { ?>
-            <div style="width:350px;">
-                <img src="<?php echo $content?>" />
-                <br />
-                <b>File Type:</b>
-                <?php echo $filetype ?><br />
-                <b>Author:</b>
-                <?php echo $author ?><br />
-                <b>Publisher</b>
-                <?php echo $publisher ?><br />
-                <b>Date Published:</b>
-                <?php echo $date ?><br />
-                                <b>Skill Tags:</b>
-                <?php echo $skilltags ?><br />
-                                <b>Title:</b>
-                <?php echo $title ?><br />
-                                <b>Duration:</b>
-                <?php echo $duration ?><br />
-                                <b>Intent:</b>
-                <?php echo $intent ?><br />
+            <div style="width: 350px;">
+                <?php if ($filetype == "LINK") { ?>
+                    <!-- Vue component for LINK -->
+                    <div id="app">
+                        <iframe-component :src=" '<?php echo $content ?>' "></iframe-component>
+                    </div>
+                <?php } elseif ($filetype == "IMG") { ?>
+                    <!-- Vue component for IMG -->
+                    <div id="app">
+                        <iframe-component :src="'media/<?php echo $content ?>'"></iframe-component>
+                    </div>
+                <?php } elseif ($filetype == 'MP3') { ?>
+                    <!-- Vue component for MP3 -->
+                    <div id="app">
+                        <iframe-component :src="'media/<?php echo $content ?>'"></iframe-component>
+                    </div>
+                <?php } elseif ($filetype == "MP4") { ?>
+                    <!-- Vue component for MP4 -->
+                    <div id="app">
+                        <iframe-component :src="'media/<?php echo $content ?>'"></iframe-component>
+                    </div>
+                <?php } elseif ($filetype == "pdf") { ?>
+                    <!-- Vue component for PDF -->
+                    <div id="app">
+                        <iframe-component :src="'media/<?php echo $content ?>'"></iframe-component>
+                    </div>
+                <?php } else { ?>
+                    <!-- Vue component for default (fallback) -->
+                    <div id="app">
+                        <iframe-component :src="'<?php echo $content ?>'"></iframe-component>
+                    </div>
+                <?php } ?>
             </div>
 
-            
-        <?php } ?>
+            <script>
+                // Define the Vue iframe component
+                Vue.component('iframe-component', {
+                    props: ['src'],
+                    template: '<iframe :src="src"></iframe>'
+                });
+
+                // Initialize the Vue app
+                new Vue({
+                    el: '#app',
+                });
+            </script>
+
+
+            <br />
+            <b>File Type:</b>
+            <?php echo $filetype ?><br />
+            <b>Author:</b>
+            <?php echo $author ?><br />
+            <b>Publisher</b>
+            <?php echo $publisher ?><br />
+            <b>Date Published:</b>
+            <?php echo $date ?><br />
+            <b>Skill Tags:</b>
+            <?php echo $skilltags ?><br />
+            <b>Title:</b>
+            <?php echo $title ?><br />
+            <b>Duration:</b>
+            <?php echo $duration ?><br />
+            <b>Intent:</b>
+            <?php echo $intent ?><br />
+        </div>
+    <?php } ?>
 </body>
 
 </html>
--- home.php
+++ home.php
@@ -16,31 +16,29 @@ while ($row = mysqli_fetch_array($result)) {
     <link href="style.css" rel="stylesheet" type="text/css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"></script>
+    <!--font awesome css-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-    <title>Hotel List</title>
+    <!--stylesheet-->
+    <link rel="stylesheet" href="style.css">
+    <title>FYP prototype</title>
 </head>
 
 <body>
     <div class="row">
-        <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="home.php">Hotels</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#collapsibleNavbar">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="collapsibleNavbar">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link" href="details copy.php">Hotel Reviews</a>
-                        </li>
-                    </ul>
-                </div>
-        </nav>
-        <h1>List of hotels</h1>
-        <table border="2" cellpadding="2" cellspacing="2">
+        <ul class="topnav" id="myTopnav">
+            <li><img src="media/osmosis logo.png" alt="osmosis learn logo" class="logo" width="250" height="80"></li>
+            <li><a href="#explore" class="explore">Explore</a></li>
+            <li><a href="#create" class="create">Create</a></li>
+            <li><a href="#events" class="events">Events</a></li>
+            <li><a href="#login" class="login">Log In</a></li>
+            <li><a href="#signup" class="signup">Sign Up</a></li>
+        </ul>
+        <hr>
+        <h1>Explore Assets</h1>
+        <hr>
+        <table cellpadding="2" cellspacing="2">
             <tr>
-                <th>Thumbnail</th>
+                <th>Image</th>
                 <th>Name</th>
                 <th>File Type</th>
                 <th>iFrame</th>
@@ -64,8 +62,7 @@ while ($row = mysqli_fetch_array($result)) {
                 ?>
 
                 <tr>
-                    <td><img
-                            src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($picture); ?>" />
+                    <td><img src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($picture); ?>" />
                     </td>
                     <td>
                         <?php echo $title; ?>
@@ -73,7 +70,7 @@ while ($row = mysqli_fetch_array($result)) {
                     <td>
                         <?php echo $filetype; ?>
                     </td>
-                    <td><a href="details copy.php?asset_id=<?php echo $id; ?>">Click Here</a></td>
+                    <td><a href="details.php?asset_id=<?php echo $id; ?>">Click Here</a></td>
                     <td>
                         <?php echo $duration; ?>
                     </td>
@@ -91,6 +88,7 @@ while ($row = mysqli_fetch_array($result)) {
             }
             ?>
         </table>
+    </div>
 </body>
 
 </html>
