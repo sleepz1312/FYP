ea7f98f6609c5372b23ae2994d1736bbafd2acfd
jiaenlow-rp <21024625@myrp.edu.sg>
2023-08-08 16:55:40 +0800

    padding

--- navBar.php
+++ navBar.php
@@ -1,15 +1,13 @@
 <nav class="navbar navbar-expand-md navbar-light bg-white ">
     <div id="menu" class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2 container-fluid">
         <ul class="navbar-nav mr-auto">
-            <center><a class="navbar-brand mx-auto" href="home.php"><img src="media/osmosis learn logo.png" alt="Logo"
-                        width="250" height="80" /></a></center>
+            <center><a class="navbar-brand mx-auto" href="home.php"><img src="media/osmosis learn logo.png" alt="Logo" id="logo"
+                    style='padding-left:50px' width="120%" height="80" /></a></center>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
-            <li class="nav-item">
-                <a class="nav-link" href="home.php">Home</a>
-            </li>
+
         </ul>
 
         <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
@@ -21,7 +19,7 @@
                 <?php } ?>
                 <li><a href="#explore" class="explore">Explore</a></li>
                 <li> <a class="create" style="color:lightgray">Create</a></li>
-                <li> <a class="events" style="color: lightgray">Events</a></li>
+                <li> <a class="events" style="color: lightgray">Events</a< /li>
             </ul>
         </div>
     </div>
